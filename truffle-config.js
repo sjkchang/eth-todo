341b8f274889976ebd0751e280c9449be40f9303
sjkchang <sjkchang@gmail.com>
2022-05-03 20:07:47 -0700

    First Commit. Setup truffle React Project

---
+++ truffle-config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    develop: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id    }
+    }
+  },
+  solc: {
+      optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
+}
